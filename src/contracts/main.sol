f524f3147ecc7229278e59757fb7ad918d99de5b
zelbov <jimborecard@gmail.com>
2022-04-04 01:22:27 +0300

    Added OpenSea integration features from Tradeable

--- src/contracts/main.sol
+++ src/contracts/main.sol
@@ -6,17 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "./ERC721Tradable.sol";
+
 /**
  * @title Cannasapience NFT contract base
  */
-contract __CONTRACT_NAME__ is ERC721 {
+contract __CONTRACT_NAME__ is TradeableERC721Token {
     using Counters for Counters.Counter;
 
     Counters.Counter private currentTokenId;
 
-    constructor() ERC721("__TOKEN_NAME__", "__TOKEN_SYMBOL__") {}
+    constructor(address _proxyRegistryAddress) TradeableERC721Token("__TOKEN_NAME__", "__TOKEN_SYMBOL__", _proxyRegistryAddress) public {}
 
-    function _baseURI() internal view virtual override returns (string memory) {
+    function baseTokenURI() public view returns (string memory) {
         return "__ERC721_BASE_TOKEN_URI__";
     }
 
