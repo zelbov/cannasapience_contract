0cf55472b3aad9f1913f034906fea9e7a5e1ee07
zelbov <jimborecard@gmail.com>
2022-04-04 05:55:20 +0300

    Fixed imported contract base & resolve paths

--- src/contracts/main.sol
+++ src/contracts/main.sol
@@ -11,15 +11,22 @@ import "./ERC721Tradable.sol";
 /**
  * @title Cannasapience NFT contract base
  */
-contract __CONTRACT_NAME__ is TradeableERC721Token {
+contract __CONTRACT_NAME__ is ERC721Tradable {
     using Counters for Counters.Counter;
 
     Counters.Counter private currentTokenId;
 
-    constructor(address _proxyRegistryAddress) TradeableERC721Token("__TOKEN_NAME__", "__TOKEN_SYMBOL__", _proxyRegistryAddress) public {}
+    constructor(address _proxyRegistryAddress) ERC721Tradable("__TOKEN_NAME__", "__TOKEN_SYMBOL__", _proxyRegistryAddress) public {}
 
     function baseTokenURI() public view returns (string memory) {
         return "__ERC721_BASE_TOKEN_URI__";
+    }
+
+    function tokenURI(uint256 _tokenId) public view returns (string) {
+        return Strings.strConcat(
+            baseTokenURI(),
+            Strings.uint2str(_tokenId)
+        );
     }
 
 }
