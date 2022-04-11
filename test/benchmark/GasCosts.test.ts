7e1fed89ecc0352d3a78264d6ed5f994a0432315
zelbov <jimborecard@gmail.com>
2022-04-11 11:47:45 +0300

    Tested gas costs of public sale mints

--- test/benchmark/GasCosts.test.ts
+++ test/benchmark/GasCosts.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import 'mocha/mocha'
 import { join } from 'path'
+import { setTimeout } from 'timers/promises'
 import { compileSolidityContract } from '../../src/modules/ContractCompiler'
 import { EthRPC } from '../../src/modules/EthRPC'
 import { EthAccount } from '../../src/types/EthAccount'
@@ -26,6 +27,9 @@ describe('Gas costs performance', () => {
     })
 
     before('Compile main contract', async function(){
+
+        // override presale duration to prevent long waits
+        process.env.PRESALE_DURATION_SECONDS = '60'
 
         this.timeout(0)
 
@@ -65,6 +69,8 @@ describe('Gas costs performance', () => {
 
         let deployed = false
 
+        let presaleEndTS : number = 0;
+
         it('Deployment', async function(){
 
             const deployTx = await rpc.prepareSmartContractDeployTransaction(
@@ -81,6 +87,11 @@ describe('Gas costs performance', () => {
             report('Deployment gas used:', deployResult.gasUsed)
 
             expect(deployTx.gas).lte(5_000_000)
+
+            // record presale end timestamp
+            presaleEndTS = await rpc.getContractPropertyValue(
+                contract, contractAddress, 'publicSaleStartTime', root.address
+            )
 
         })
 
@@ -160,8 +171,6 @@ describe('Gas costs performance', () => {
 
         })
 
-        it('Token minting at public sale')
-
         it('Airdropping', async function(){
 
             if(!deployed) return this.skip()
@@ -211,6 +220,94 @@ describe('Gas costs performance', () => {
             expect(rcpt.status).eq(true)
 
             report('Tx WITHDRAW: gas used:', rcpt.gasUsed, 'of', gas, 'estimated');
+
+        })
+
+        describe('Token minting at public sale', () => {
+
+            const listed : string[] = [], listedAmount = +process.env.MAX_USER_MINTED_TOKENS_PER_TX! + 2;
+
+            before(async function(){
+
+                if(!deployed) return this.skip()
+            
+                this.timeout(0)
+
+                // wait for presale period ends
+
+                const waitTime = (presaleEndTS - Math.floor(new Date().getTime() / 1000)) * 1000
+
+                console.log('Waiting for', Math.round(waitTime / 1000), 'seconds for public sale to start...')
+
+                await setTimeout(waitTime)
+
+                // refill other users balance, and so, seal new block for block.timestamp update
+
+                for(let i = 0; i < listedAmount; i++) {
+
+                    const wallet = await rpc.createAccount()
+                    listed.push(wallet.privateKey)
+                    await rpc.sendETH(root.privateKey, wallet.address, '0.1');
+        
+                }
+
+                // check whether it is a public sale already
+
+                const isPublicSale = await rpc.getContractPropertyValue(
+                    contract, contractAddress, 'isPublicSale', root.address
+                )
+
+                expect(isPublicSale).eq(true)
+
+            })
+
+            for(let amount = 1; amount <= +process.env.MAX_USER_MINTED_TOKENS_PER_TX!; amount++)
+            it('Token minting at public sale: '+amount+' tokens per tx', async function(){
+
+                if(!deployed) return this.skip()
+
+                this.timeout(0);
+
+                const mintPrice = process.env.SALE_TOKEN_PRICE_ETH!.replace(' ether', '')
+
+                report('Mint price:', mintPrice, 'ETH')
+
+                const mintPriceValue = rpc.client.utils.toWei(mintPrice, 'ether')
+
+                const tokensPerTx = amount;
+
+                let idx = 0;
+
+                let tokensToMint = amount, maxMinted = +process.env.MAX_PUBLIC_SALE_TOKENS_MINT!
+
+                const initiatorPkey = listed[amount]
+
+                for(let i = 0; i < maxMinted && idx < 3; i += tokensToMint) {
+
+                    idx++;
+
+                    if(idx * tokensToMint > maxMinted) tokensToMint -= idx * tokensToMint - maxMinted
+
+                    const { signed, gas } = await rpc.prepareContractCallTransaction(
+                        contract,
+                        contractAddress,
+                        'mintTokens',
+                        [tokensToMint],
+                        initiatorPkey,
+                        (Math.ceil(+mintPriceValue * tokensPerTx)).toString()
+                    );
+                        
+                    const rcpt = await rpc.sendContractCallTransaction(signed)
+
+                    expect(rcpt.status).eq(true);
+                    report(
+                        'Tx #',idx,': gas used:',rcpt.gasUsed,'of',gas,'estimated.', 
+                        'Minted',tokensToMint,'tokens'
+                    );
+
+                }
+
+            })
 
         })
 
